99d9a3beb554655a7a25559a261067df912db405
Pedro <pedrobastarrica@gmail.com>
2018-11-17 19:08:51 -0300

    Updates

---
+++ precache-manifest.684b7e1431375c46bf1a9ec8e3a65882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a30fbffb6d1319e2217",
+    "url": "/website/static/css/main.fa3754ca.chunk.css"
+  },
+  {
+    "revision": "4a30fbffb6d1319e2217",
+    "url": "/website/static/js/main.4a30fbff.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/website/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6951a987701ea75b5657606faa85fe55",
+    "url": "/website/index.html"
+  }
+];
